9f864c36e54ec8bfdf1c3a0ba24a5df6ebc9bf70
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-01-06 10:47:24 +0100

    Add project files.

---
+++ Html.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TableCreator
+{
+    public static class Html
+    {
+        public class Table : HtmlBase, IDisposable
+        {
+            public Table(StringBuilder sb, List<string> classAttributes = null, string id = "") : base(sb)
+            {
+                Append("<table");
+                AddOptionalAttributes(classAttributes, id);
+            }
+
+            public void StartHead(List<string> classAttributes = null, string id = "")
+            {
+                Append("<thead");
+                AddOptionalAttributes(classAttributes, id);
+            }
+
+            public void EndHead()
+            {
+                Append("</thead>");
+            }
+
+            public void StartFoot(List<string> classAttributes = null, string id = "")
+            {
+                Append("<tfoot");
+                AddOptionalAttributes(classAttributes, id);
+            }
+
+            public void EndFoot()
+            {
+                Append("</tfoot>");
+            }
+
+            public void StartBody(List<string> classAttributes = null, string id = "")
+            {
+                Append("<tbody");
+                AddOptionalAttributes(classAttributes, id);
+            }
+
+            public void EndBody()
+            {
+                Append("</tbody>");
+            }
+
+            public void Dispose()
+            {
+                Append("</table>");
+            }
+
+            public Row AddRow(List<string> classAttributes, string id = "")
+            {
+                return new Row(GetBuilder(), classAttributes, id);
+            }
+        }
+
+        public class Row : HtmlBase, IDisposable
+        {
+            public Row(StringBuilder sb, List<string> classAttributes, string id = "") : base(sb)
+            {
+                Append("<tr");
+                AddOptionalAttributes(classAttributes, id);
+            }
+            public void Dispose()
+            {
+                Append("</tr>");
+            }
+            public void AddCell(string innerText, List<string> classAttributes = null, string id = "", string colSpan = "")
+            {
+                Append("<td");
+                AddOptionalAttributes(classAttributes, id, colSpan);
+                Append(innerText);
+                Append("</td>");
+            }
+            public void AddActionCell(string entityId, List<string> classAttributes = null, string id = "", string colSpan = "")
+            {
+                Append("<td");
+                AddOptionalAttributes(classAttributes, id, colSpan);
+                Append($"<a class='btn btn-warning mr-1 text-white update-btn' data-toggle='modal' data-id='{entityId}'> <i class='fas fa-pen'></i></a>");
+                Append($"<a class='btn btn-danger text-white delete-btn' data-toggle='modal' data-id='{entityId}'> <i class='fas fa-trash'></i></a>");
+                Append("</td>");
+            }
+        }
+
+        public abstract class HtmlBase
+        {
+            private StringBuilder _sb;
+
+            protected HtmlBase(StringBuilder sb)
+            {
+                _sb = sb;
+            }
+
+            public StringBuilder GetBuilder()
+            {
+                return _sb;
+            }
+
+            protected void Append(string toAppend)
+            {
+                _sb.Append(toAppend);
+            }
+
+            protected void AddOptionalAttributes(List<string> classes = null, string id = "", string colSpan = "")
+            {
+
+                if (!string.IsNullOrEmpty(id))
+                {
+                    _sb.Append($" id='{id}'");
+                }
+
+
+                if (classes != null)
+                {
+                    _sb.Append($" class='{string.Join(" ", classes)}'");
+                }
+
+
+                if (!string.IsNullOrEmpty(colSpan))
+                {
+                    _sb.Append($" colspan='{colSpan}'");
+                }
+                _sb.Append(">");
+            }
+        }
+    }
+}
---
+++ TableFromObject.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+
+namespace TableCreator
+{
+    public static class TableFromObject<T> where T : class
+    {
+        private static List<string> tableClasses = new List<string>() { "table", "table-bordered", "outer-shadow" };
+        private static List<string> theadClasses = new List<string>() { "table-head" };
+        private static List<string> dNone = new List<string>() { "d-none" };
+
+        public static string CreateTable(List<T> model)
+        {
+            StringBuilder sb = new StringBuilder();
+            var headers = GetDisplayNames(model[0]);
+            using (Html.Table table = new Html.Table(sb, classAttributes: tableClasses, id: ""))
+            {
+                table.StartHead(theadClasses);
+                using (var thead = table.AddRow(new List<string>()))
+                {
+                    foreach (var header in headers)
+                    {
+                        thead.AddCell(header, classAttributes: theadClasses);
+                    }
+                    thead.AddCell("Actions", classAttributes: theadClasses);
+                }
+                table.EndHead();
+                table.StartBody();
+                foreach (var item in model)
+                {
+                    var id = "";
+                    var value = "";
+                    using (var tr = table.AddRow(classAttributes: new List<string>()))
+                    {
+                        foreach (PropertyInfo prop in item.GetType().GetProperties())
+                        {
+                            if (prop.Name != "Id")
+                            {
+                                if (prop.PropertyType == typeof(DateTime))
+                                {
+                                    value = ((DateTime)prop.GetValue(item, null)).ToShortDateString();
+                                }
+                                else
+                                {
+                                    value = prop.GetValue(item, null) != null ? prop.GetValue(item, null).ToString() : string.Empty;
+                                }
+                                value.Replace("<", "");
+                                value.Replace(">", "");
+                                tr.AddCell(value.Length < 50 ? value : value.Substring(0, 50) + " ...");
+                            }
+                            else
+                            {
+                                tr.AddCell(prop.GetValue(item, null).ToString(), classAttributes: dNone);
+                                id = prop.GetValue(item, null).ToString();
+                            }
+
+                        }
+                        tr.AddActionCell(id);
+
+                    }
+
+                }
+                table.EndBody();
+            }
+            sb.Replace("<script>", "");
+            sb.Replace("</script>", "");
+            return sb.ToString();
+        }
+
+
+        private static List<string> GetDisplayNames(T model)
+        {
+            var list = new List<string>();
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var prop in Props)
+            {
+                if (prop.Name != "Id")
+                {
+
+                    DisplayAttribute dna = (DisplayAttribute)Attribute.GetCustomAttribute(prop, typeof(DisplayAttribute));
+                    if (dna != null)
+                        list.Add(dna.Name);
+                    else
+                        list.Add(prop.Name);
+
+                }
+            }
+
+            return list;
+        }
+    }
+}
